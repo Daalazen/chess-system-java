f5ac1d8f754a042225f70f88db05765917bd9586
Daalazen <lucasdalazen07@gmail.com>
2024-03-11 22:00:18 -0300

    começando a implementar Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position position = new Position(10,10);
-		
-		System.out.println(position);
+		Board board = new Board(10, 10);
 
+		
 	}
 
 }
